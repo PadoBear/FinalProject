117deec02a001b9678065cb142d7efdceda246c6
PadoBear <96169555+PadoBear@users.noreply.github.com>
2021-12-18 19:05:26 +0800

    更新 pxt.json, main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts

--- main.ts
+++ main.ts
@@ -1,8 +1,37 @@
+namespace SpriteKind {
+    export const res = SpriteKind.create()
+}
 controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
     if (mySprite.vy == 0) {
         mySprite.vy = -100
     }
 })
+sprites.onOverlap(SpriteKind.Player, SpriteKind.res, function (sprite, otherSprite) {
+    otherSprite.destroy(effects.spray, 500)
+    for (let index = 0; index <= 4; index++) {
+        projectile = sprites.createProjectileFromSide(img`
+            d d d d d d d d d d d d d d d d 
+            d d d d d d d d d d d d d d d d 
+            d d d d d d d d d d d d d d d d 
+            d d d d d d d d d d d d d d d d 
+            d d d d d d d d d d d d d d d d 
+            d d d d d d d d d d d d d d d d 
+            d d d d d d d d d d d d d d d d 
+            d d d d d d d d d d d d d d d d 
+            d d d d d d d d d d d d d d d d 
+            d d d d d d d d d d d d d d d d 
+            d d d d d d d d d d d d d d d d 
+            d d d d d d d d d d d d d d d d 
+            d d d d d d d d d d d d d d d d 
+            d d d d d d d d d d d d d d d d 
+            d d d d d d d d d d d d d d d d 
+            d d d d d d d d d d d d d d d d 
+            `, 0, 200)
+        projectile.setPosition(mySprite.x, mySprite.y - 50)
+    }
+})
+let projectile: Sprite = null
+let FLOWER: Sprite = null
 let mySprite: Sprite = null
 mySprite = sprites.create(img`
     ........................
@@ -36,6 +65,28 @@ scene.cameraFollowSprite(mySprite)
 scene.setBackgroundColor(9)
 tiles.setTilemap(tilemap`層級1`)
 for (let value of tiles.getTilesByType(assets.tile`myTile0`)) {
-    tiles.placeOnRandomTile(mySprite, assets.tile`myTile0`)
+    tiles.placeOnTile(mySprite, value)
+    tiles.setTileAt(value, assets.tile`transparency16`)
+}
+for (let value of tiles.getTilesByType(assets.tile`myTile1`)) {
+    FLOWER = sprites.create(img`
+        3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 
+        3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 
+        3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 
+        3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 
+        3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 
+        3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 
+        3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 
+        3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 
+        3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 
+        3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 
+        3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 
+        3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 
+        3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 
+        3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 
+        3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 
+        3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 
+        `, SpriteKind.res)
+    tiles.placeOnTile(FLOWER, value)
     tiles.setTileAt(value, assets.tile`transparency16`)
 }
